a2355f7c4adde71454aebb15b4b6d043fd0f16ef
Vidyadhar Thatte <vd_thatte@yahoo.com>
2016-11-22 12:07:57 -0500

    setup imports

---
+++ nov22-2016/linear_regression.py
@@ -0,0 +1,17 @@
+"""
+
+devname: vdthatte
+
+using this tutorial
+https://github.com/justmarkham/DAT4/blob/master/notebooks/08_linear_regression.ipynb
+
+
+"""
+
+# imports
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+

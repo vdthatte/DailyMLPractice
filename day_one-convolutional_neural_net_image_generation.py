14839efa5246c3d7fbe5df9d8ad4a3cb81b29e6e
Vidyadhar Thatte <vd_thatte@yahoo.com>
2016-11-21 22:10:24 -0500

    initial commit

---
+++ day_one-convolutional_neural_net_image_generation.py
@@ -0,0 +1,19 @@
+"""
+
+dev name: vdthatte
+LET'S goooooooo
+
+"""
+
+
+# boilerplate code
+from __future__ import print_function
+import os
+from io import BytesIO
+import numpy as np
+from functools import partial
+import PIL.Image
+from IPython.display import clear_output, Image, display, HTML
+
+import tensorflow as tf
+

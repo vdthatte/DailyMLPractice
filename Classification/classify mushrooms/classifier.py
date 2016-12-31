1c25d4f1f486a6e9bbdc7bc59817c5616791ecb0
Vidyadhar Thatte <vd_thatte@yahoo.com>
2016-12-31 12:13:22 -0500

    errors

---
+++ Classification/classify mushrooms/classifier.py
@@ -0,0 +1,50 @@
+# load the data
+from sklearn.cross_validation import train_test_split
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+import csv
+
+with open('mushrooms.csv', 'rb') as csvfile:
+	rows = []
+	mushrooms = csv.reader(csvfile, delimiter=' ', quotechar='|')
+	for row in mushrooms:
+		rows.append(row[0].split(','))
+
+	#print rows[1][1:] 
+
+	x = []
+	y = []
+
+	for row in rows:
+		y.append(row[0])
+		x.append(row[1:])
+	
+	y = y[1:]
+	x = x[1:]
+
+	# building my own encoder
+	encoded_y = []
+	encoded_x = []
+
+	for letter in y:
+		print (format(ord(letter), 'b'))
+
+	for theList in x:
+		newList = []
+		for letter in theList:
+			newList.append(format(ord(letter), 'b'))
+		encoded_y.append(newList)
+
+	X_train, X_test, y_train, y_test = train_test_split(encoded_x, encoded_y, test_size = 0.1)
+
+	# train the classifier - decision tree
+
+	my_classifier = tree.DecisionTreeClassifier()
+	my_classifier.fit(X_train, y_train)
+
+
+	# make a prediction using test data
+	predictions = my_classifier.predict(X_test)
+	print predictions

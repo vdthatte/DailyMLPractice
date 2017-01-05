84656dc506c4a2d5ac1ac7d99657b3b4c0dceca4
Vidyadhar Thatte <vd_thatte@yahoo.com>
2017-01-05 12:01:46 -0500

    gotta finish this later

---
+++ tflearn/linearRegression.py
@@ -0,0 +1,13 @@
+## install tflearn and tensorflow
+
+from __future__ import absolute_import, division, print_function
+
+import tflearn
+
+X = [3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,7.042,10.791,5.313,7.997,5.654,9.27,3.1]
+Y = [1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,2.827,3.465,1.65,2.904,2.42,2.94,1.3]
+
+input_ = tflearn.input_data(shape=[None])
+linear = tflearn.single_unit(input_)
+regression = tflearn.regression(linear, optimizer='sgd', loss='mean_square',
+                                metric='R2', learning_rate=0.01)

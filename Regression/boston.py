a804bb93fdbeb2c5f0c0b22f0b616ddcc087d32b
Vidyadhar Thatte <vd_thatte@yahoo.com>
2017-01-05 11:48:13 -0500

    update

--- Regression/boston.py
+++ Regression/boston.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_boston
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn import linear_model
 
 boston = load_boston()
 
@@ -9,3 +10,7 @@ x = boston.data
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.5)
 
+regr = linear_model.LinearRegression()
+regr.fit(X_train, y_train)
+
+print regr.predict(X_test)
